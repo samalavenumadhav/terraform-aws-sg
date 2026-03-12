e0c9b8b7ebf3c9a6ad583012dcf6614c4dac68f0
venu madhav <madhavsamala171@gmail.com>
2026-03-12 20:51:49 +0530

    A

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "main" {
   name        = "${var.project}-${var.environment}-${var.sg_name}"
   description = "Allow TLS inbound traffic for ${var.project} in ${var.environment} for component ${var.sg_name}"
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
 
   egress {
     from_port        = 0
@@ -9,14 +9,12 @@ resource "aws_security_group" "main" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-
-
-   tags = merge (
-         var.sg_tags,
-        local.common_tags,
-        {
-            Name = "${var.project}-${var.environment}-${var.sg_name}"
-        
-        }
-    )
+  
+  tags = merge(
+    var.sg_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-${var.sg_name}"
+    }
+  )
 }
