b9071b78d638ed17c9efacf32fbfc43a2c1ecb84
venu madhav <madhavsamala171@gmail.com>
2026-03-12 19:29:26 +0530

    A

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "main" {
   name        = "${var.project}-${var.environment}-${var.sg_name}"
-  description = "Allow TLS inbound traffic for ${var.sg_name}"
+  description = "Allow TLS inbound traffic for ${var.project} in ${var.environment} for component ${var.sg_name}"
   vpc_id = var.vpc_id
 
   egress {
